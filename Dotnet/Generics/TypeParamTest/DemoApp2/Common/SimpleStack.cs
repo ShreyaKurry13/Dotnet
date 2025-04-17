2fd9839e47371e1c8bbe53def8d6f6ab471eca70
ShreyaKurry13 <ShreyaKurry13@gmail.com>
2025-04-17 21:14:28 +0530

    updated

---
+++ Dotnet/Generics/TypeParamTest/DemoApp2/Common/SimpleStack.cs
@@ -0,0 +1,31 @@
+namespace Common;
+
+public class SimpleStack<E>
+{
+    class Node
+    {
+        internal E element;
+        internal Node below;
+    }
+
+    private Node top;
+
+    public void Push(E item)
+    {
+        top = new Node {element = item, below = top};
+    }
+
+    public E Pop()
+    {
+        Node temp = top;
+        top = top.below;
+        return temp.element;
+    }
+
+    public bool isEmpty()
+    {
+        return top is null;
+    }
+
+    
+}
