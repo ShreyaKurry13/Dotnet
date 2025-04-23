7d9c978e3d9c5783cc5ac091fd7568480619a390
ShreyaKurry13 <ShreyaKurry13@gmail.com>
2025-04-23 15:52:54 +0530

    updated

---
+++ Dotnet/Fundamentals/Functional/DelegateTest/DemoApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace DemoApp;
+
+class Program
+{
+    static double SafeSchema(int period)
+    {
+        return period < 3 ? 5.5 : 6;
+    }
+
+    static void Main(string[] args)
+    {
+        double p = double.Parse(args[0]);
+        int n = int.Parse(args[1]);
+
+        var a = new Investment(p, n);
+        Console.WriteLine("Future value for riskless investment: {0:0.00}",a.FutureValue(SafeSchema));
+        Console.WriteLine("Future value for riskful investment: {0:0.00}",a.FutureValue(y => 8 + 0.25 * y));
+
+    }
+}
