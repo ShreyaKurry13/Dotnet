7580bcf662f56a573ba5d321afdaf3f8e58eeeaf
ShreyaKurry13 <ShreyaKurry13@gmail.com>
2025-04-30 20:24:24 +0530

    updated

--- Dotnet/Advanced/AspNetCoreMvc/RazorPagesTest/DemoApp1/Program.cs
+++ Dotnet/Advanced/AspNetCoreMvc/RazorPagesTest/DemoApp1/Program.cs
@@ -1,5 +1,8 @@
+using DemoApp.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
+builder.Services.AddSingleton<IVisitCounter,PersonalCounter>();
 
 var app = builder.Build();
 app.MapRazorPages();
